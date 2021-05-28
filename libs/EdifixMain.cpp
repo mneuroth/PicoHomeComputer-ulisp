6a13844717da9be34a6ba01ac3f134236740b4af
Michael Neuroth <michael.neuroth@freenet.de>
2021-05-28 22:00:12 +0200

    Fixed line endings

--- libs/EdifixMain.cpp
+++ libs/EdifixMain.cpp
@@ -1,1 +1,11 @@
-#include "EdifixEditor.h"extern "C" int __cdecl main(int argc, char *argv[]){    EdifixEditor aEditor("test main", 0);        aEditor.Run();        return 0;}
+
+#include "EdifixEditor.h"
+
+extern "C" int __cdecl main(int argc, char *argv[])
+{
+    EdifixEditor aEditor("test main", 0);
+    
+    aEditor.Run();
+    
+    return 0;
+}
